17c48a2c1775fa7e0bc3ca2b1ba47e57082a1a03
mathesukkj <kemuel.g7363@gmail.com>
2024-06-02 16:00:04 -0300

    feat: finally make the ws server work

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,7 +1,26 @@
 package main
 
-import "golang.org/x/net/websocket"
+import (
+	"io"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+func NewWsServer(ws *websocket.Conn) {
+	io.Copy(ws, ws)
+}
+
+func HandleWs(w http.ResponseWriter, req *http.Request) {
+	s := websocket.Server{Handler: websocket.Handler(NewWsServer)}
+	s.ServeHTTP(w, req)
+}
 
 func main() {
-	server := websocket.DialConfig
+	http.HandleFunc("/", HandleWs)
+
+	err := http.ListenAndServe(":8020", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
 }
